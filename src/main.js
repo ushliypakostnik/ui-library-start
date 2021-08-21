67b83f5859eba9fef9b6237d775f6fd681d40137
ushliypakostnik <levon.gambaryan@gmail.com>
2021-08-21 13:39:30 +0300

    fixes-fixes

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 
 import ComponentLibrary from 'ui-library-starter';
-
 import 'ui-library-starter/dist/ui-library-starter.css';
 
 Vue.use(ComponentLibrary);
